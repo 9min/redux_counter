9a5cb624b0fb831dc5dc3eda3881a22f89ef584c
9min <gm0711@nate.com>
2018-08-13 10:57:38 +0900

    CRATE -> CREATE 오타 수정
    CRATE -> CREATE 오타 수정

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,5 +1,5 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const SET_COLOR = 'SET_COLOR';
-export const CRATE = 'CREATE';
+export const CREATE = 'CREATE';
 export const REMOVE = 'REMOVE';
